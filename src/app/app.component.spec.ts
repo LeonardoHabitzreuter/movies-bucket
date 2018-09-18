04a0e576fca58bb709e2e599b25a2cf2a663ebc2
Leonardo Habitzreuter <LeonardoHabitzreuter@senior.com.br>
2018-09-18 08:21:34 -0300

    fixed the tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,48 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from '@angular/core/testing'
+import { Component } from '@angular/core'
+import { AppComponent } from './app.component'
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MockTableComponent,
+        MockModalComponent
       ],
-    }).compileComponents();
-  }));
+    }).compileComponents()
+  }))
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.debugElement.componentInstance
+    expect(app).toBeTruthy()
+  }))
+  
   it(`should have as title 'movies-bucket'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('movies-bucket');
-  }));
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.debugElement.componentInstance
+    expect(app.title).toEqual('Movies bucket')
+  }))
+  
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to movies-bucket!');
-  }));
-});
+    const fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
+    const compiled = fixture.debugElement.nativeElement
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Movies bucket!')
+  }))
+})
+
+@Component({
+  selector: 'movie-table',
+  template: ''
+})
+class MockTableComponent {
+}
+
+@Component({
+  selector: 'movie-modal',
+  template: ''
+})
+class MockModalComponent {
+}
