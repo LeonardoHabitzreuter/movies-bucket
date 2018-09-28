b438efac1812051d943750c744eb08f5b7cac3b3
Leonardo Habitzreuter <leonardohabitzreuter@senior.com.br>
2018-09-28 09:04:20 -0300

    jest is working now!

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,31 @@
 module.exports = {
-  preset: "jest-preset-angular",
-  setupTestFrameworkScriptFile: "<rootDir>/configs/tests/setupJest.ts",
-  verbose: true,
-  transform: {
+  "preset": "jest-preset-angular",
+  "setupTestFrameworkScriptFile": "<rootDir>/configs/tests/setupJest.ts",
+  "transform": {
     "^.+\\.js$": "babel-jest",
-    "^.+\\.ts$": "ts-jest"
+    '^.+\\.(ts|html)$': '<rootDir>/node_modules/jest-preset-angular/preprocessor.js'
   },
-  globals: {
-    "NODE_ENV": "test"
-  }
+  "globals": {
+    "ts-jest": {
+      "tsConfigFile": "src/tsconfig.spec.json"
+    },
+    "__TRANSFORM_HTML__": true
+  },
+  "testMatch": [
+    "**/__tests__/**/*.+(ts|js)?(x)",
+    "**/+(*.)+(spec|test).+(ts|js)?(x)"
+  ],
+  "moduleFileExtensions": [
+    "ts",
+    "js",
+    "html"
+  ],
+  "moduleNameMapper": {
+    "app/(.*)": "<rootDir>/src/app/$1",
+    "assets/(.*)": "<rootDir>/src/assets/$1",
+    "environments/(.*)": "<rootDir>/src/environments/$1"
+  },
+  "transformIgnorePatterns": [
+    "node_modules/(?!@ngrx)"
+  ]
 }
